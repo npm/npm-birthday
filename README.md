a88e937b9642b11a75c31d186dfae6ff0d7ec100
nlf <quitlahok@gmail.com>
2021-12-02 14:27:24 -0800

    feat: rewrite in human readable code

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 Implementation of the "npm birthday" command
 
-Abstracted out to a separate module so that this highly optimized function
-doesn't trip automated code analysis alarms.
+~Abstracted out to a separate module so that this highly optimized function
+doesn't trip automated code analysis alarms.~
+
+This was formerly an obfuscated easter egg, but for obvious reasons that was a
+tough sell. It has now been rewritten to be human readable code to make our
+users more comfortable. The command in its entirety is also scheduled to be
+removed from `npm` with version 9.
---
+++ test/index.js
@@ -0,0 +1,116 @@
+const t = require('tap')
+const requireInject = require('require-inject')
+
+t.test('birthday (nope)', (t) => {
+  t.plan(1)
+
+  const RealDate = Date
+  const consoleLog = console.log
+  t.teardown(() => {
+    global.Date = RealDate
+    console.log = consoleLog
+  })
+
+  class FakeDate extends Date {
+    getUTCMonth() {
+      return 7
+    }
+  }
+
+  global.Date = FakeDate
+  console.log = () => undefined
+
+  const birthday = t.mock('../', {})
+  birthday((err) => {
+    t.match(err, 'try again', 'not telling you the secret that easily are we?')
+  })
+})
+
+t.test('birthday (nope again)', (t) => {
+  t.plan(1)
+
+  const RealDate = Date
+  const consoleLog = console.log
+  t.teardown(() => {
+    global.Date = RealDate
+    console.log = consoleLog
+  })
+
+  class FakeDate extends Date {
+    getFullYear() {
+      const now = new RealDate()
+      return now.getFullYear() + 1
+    }
+
+    getUTCMonth() {
+      return 9
+    }
+  }
+
+  global.Date = FakeDate
+  console.log = () => undefined
+
+  const birthday = t.mock('../', {})
+  birthday((err) => {
+    t.match(err, 'try again', 'not telling you the secret that easily are we?')
+  })
+})
+
+t.test('birthday (strike 3)', (t) => {
+  t.plan(1)
+
+  const RealDate = Date
+  const consoleLog = console.log
+  t.teardown(() => {
+    global.Date = RealDate
+    console.log = consoleLog
+  })
+
+  class FakeDate extends Date {
+    getFullYear() {
+      const now = new RealDate()
+      return now.getFullYear() - 1
+    }
+
+    getUTCMonth() {
+      return 11
+    }
+  }
+
+  global.Date = FakeDate
+  console.log = () => undefined
+
+  const birthday = t.mock('../', {})
+  birthday((err) => {
+    t.match(err, 'try again', 'not telling you the secret that easily are we?')
+  })
+})
+
+t.test('birthday (yup)', (t) => {
+  t.plan(1)
+
+  const RealDate = Date
+  const consoleLog = console.log
+  t.teardown(() => {
+    global.Date = RealDate
+    console.log = consoleLog
+  })
+
+  class FakeDate extends Date {
+    getUTCMonth() {
+      return 8
+    }
+
+    getUTCDate() {
+      return 29
+    }
+  }
+
+  global.Date = FakeDate
+  console.log = () => undefined
+
+  const birthday = t.mock('../', {})
+  birthday((err) => {
+    t.error(err, 'npm birthday')
+  })
+})
