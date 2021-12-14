5c8504464fab2df6764e31aacd4fee60fb2a0d1d
nlf <quitlahok@gmail.com>
2021-12-14 10:55:04 -0800

    feat: deobfuscate this command (#1)

--- bin.js
+++ bin.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const birthday = require('./index.js')
-const pref = Buffer.from('1B5B33373B34306D6E706D1B5B306D201B5B306D1B5B33313B34306D455252211B5B306D1B5B33356D1B5B306D20','hex')
+const pref = '\x1B[37;40mnpm\x1B[0m \x1B[0m\x1B[31;40mERR!\x1B[0m\x1B[35m\x1B[0m '
 birthday(er => {
   if (er) {
     console.log(pref + er)
